481c9152eaba23764cef939f7ed8783eaa074e4f
berb <mail@benjamin-erb.de>
2011-06-07 20:26:23 +0000

    Task Node/CouchApp  * various changes
    git-svn-id: https://svn.illmatix.de/svn/ubi/v2/external-services/task-service@1500 d2255250-64ce-4612-be3a-de8aecb87a1d

--- diretto-task-couchapp/vendor/diretto/config.js
+++ diretto-task-couchapp/vendor/diretto/config.js
@@ -1,7 +1,7 @@
 //URIs including version number: http://example/v2
 var config = {
-	"taskServiceBaseUri" : "http://taskservice/v2",
-	"coreServiceBaseUri" : "http://coreservice/v2"
+	"taskServiceBaseUri" : "http://localhost:8006/v2",
+	"coreServiceBaseUri" : "http://localhost:8001/v2"
 };
 
 if (typeof (exports) === 'object') {
--- diretto-task-node/lib/diretto/diretto-task-node/api/vote.js
+++ diretto-task-node/lib/diretto/diretto-task-node/api/vote.js
@@ -93,11 +93,106 @@ module.exports = function(h) {
 		},
 		
 		get  : function(req, res, next) {
+			var p = h.util.identifyResource(req.uriParams);
 			
+			var key;
+
+			if(p.taskId && p.submissionId && p.tagId){
+				//TODO:
+			}
+			else if(p.taskId && p.submissionId){
+				key = [p.taskId, p.submissionId];
+			}
+			else if(p.taskId && p.tagId){
+				//TODO:
+			}
+			else if(p.taskId && p.commentId){
+				key = [p.taskId, p.commentId];
+			}
+			else if(p.taskId){
+				key = ["task",p.taskId];
+			}
+			
+			if(key){
+				key.push(req.uriParams.userId);
+				h.db.view("tasks/singlevotes", {
+					key : key
+				}, function(err, dbRes) {
+					if (dbRes && dbRes.length === 1) {
+						res.send(200, {"vote":dbRes[0].value}, {});
+						next();
+					}
+					else if (dbRes) {
+						res.send(404, null, {});
+						next();
+					}
+					else {
+						res.send(500, null, {});
+						next();
+					}
+				});
+			}			
+			else{
+				res.send(404, null, {});
+				next();
+				return;
+			}		
 		},
 		
 		getAll  : function(req, res, next) {
+			var p = h.util.identifyResource(req.uriParams);
 			
+			var view;
+			var key;
+			var type;
+
+			if(p.taskId && p.submissionId && p.tagId){
+				//TODO:
+			}
+			else if(p.taskId && p.submissionId){
+				view = "tasks/submissions";
+				key = [p.taskId, p.submissionId];
+				type = "submission";
+			}
+			else if(p.taskId && p.tagId){
+				//TODO:
+			}
+			else if(p.taskId && p.commentId){
+				view = "tasks/comments";
+				key = [p.taskId, p.commentId];
+				type = "comment";
+			}
+			else if(p.taskId){
+				view = "tasks/tasks";
+				key = p.taskId;
+				type = "task";
+			}
+			
+			if(view && key){
+				h.db.view(view, {
+					key : key
+				}, function(err, dbRes) {
+					if (dbRes && dbRes.length === 1) {
+						res.send(200, dbRes[0].value.content[type].votes, {
+							"Etag" : dbRes[0].value.etag
+						});
+						next();
+					}
+					else if (dbRes) {
+						res.send(404, null, {});
+						next();
+					}
+					else {
+						res.send(500, null, {});
+						next();
+					}
+				});
+			}			
+			else{
+				res.send(404, null, {});
+				next();
+				return;
+			}
 		}
 		
 	};
